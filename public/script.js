a95d838394a916aa40a528b7373254900d7850fa
WesleySchorel <wesleyschorelhva@gmail.com>
2023-05-24 10:50:52 +0200

    Circle Progressbar Update
    Updated the progressbar on the projectboard

--- public/script.js
+++ public/script.js
@@ -3,15 +3,38 @@ function scrollFunction() {
   element.scrollIntoView({behavior: "smooth"});
 }
 
-if (window.navigator.onLine == true) {
-    document.querySelector(".online").style.display="grid";
-} else {
-    document.querySelector(".online").style.display="none";
-    document.querySelector(".offline").style.display="flex";
-}
+// if (window.navigator.onLine == true) {
+//     document.querySelector(".online").style.display="grid";
+// } else {
+//     document.querySelector(".online").style.display="none";
+//     document.querySelector(".offline").style.display="flex";
+// }
 
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
   loader.style.display = "none";
 })
 
+
+// PROGRESS
+
+const totalItems = 75; // Set the total number of items
+const progressItems = document.getElementById("progressNumber").textContent; // Set the number of items completed
+console.log(progressItems);
+const progressBar = document.querySelector('.progress-fill');
+const circumference = 251.2; // Adjusted circumference value
+const offset = circumference - (progressItems / totalItems) * circumference;
+progressBar.style.strokeDashoffset = offset;
+
+// Update color based on value range
+if (progressItems >= 0 && progressItems <= 20) {
+  progressBar.style.stroke = '#FF0000';
+} else if (progressItems >= 21 && progressItems <= 40) {
+  progressBar.style.stroke = '#FF8000';
+} else if (progressItems >= 41 && progressItems <= 60) {
+  progressBar.style.stroke = '#FFFF00';
+} else if (progressItems >= 61 && progressItems <= 75) {
+  progressBar.style.stroke = '#00FF00';
+}
+
+document.querySelector('.progress-value').textContent = progressItems; // Display the actual number of items

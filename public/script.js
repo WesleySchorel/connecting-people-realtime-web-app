d4e98ad71ddb53952e33d0d3566ed8915a7ed547
Rick Vellinga <rickvellinga2408@gmail.com>
2023-05-26 09:32:22 +0200

    fix?

--- public/script.js
+++ public/script.js
@@ -3,12 +3,17 @@ function scrollFunction() {
   element.scrollIntoView({behavior: "smooth"});
 }
 
-// if (window.navigator.onLine == true) {
-//     document.querySelector(".online").style.display="grid";
-// } else {
-//     document.querySelector(".online").style.display="none";
-//     document.querySelector(".offline").style.display="flex";
-// }
+document.getElementById("formSend").addEventListener("click", function(event){
+  event.preventDefault()
+  // console.log("hi");
+});
+
+if (window.navigator.onLine == true) {
+    document.querySelector(".online").style.display="grid";
+} else {
+    document.querySelector(".online").style.display="none";
+    document.querySelector(".offline").style.display="flex";
+}
 
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
